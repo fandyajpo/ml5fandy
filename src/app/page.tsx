531f02a8a4c3831109d5f9de54c867f3fd46f746
Fandoy <fandyglitch3@gmail.com>
2023-12-27 07:43:00 +0800

    casc

---
+++ src/app/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+// @ts-ignore
+import * as ml5 from "ml5";
+import Webcam from "react-webcam";
+("use client");
+
+export default function Home() {
+  const webcamRef = useRef<any>(null);
+  const objectDetectorRef = useRef<any>(null);
+  const [objects, setObjects] = useState<any[]>();
+
+  useEffect(() => {
+    const loadModel = async () => {
+      objectDetectorRef.current = await ml5.objectDetector(
+        "cocossd",
+        {},
+        modelLoaded
+      );
+      requestAnimationFrame(performObjectDetection);
+    };
+
+    const modelLoaded = () => {
+      console.log("Model Loaded!");
+    };
+
+    const performObjectDetection = () => {
+      if (webcamRef.current) {
+        objectDetectorRef.current.detect(
+          webcamRef.current.video,
+          (err: any, results: any) => {
+            if (err) {
+              console.error(err);
+            } else {
+              setObjects(results);
+            }
+            // Continue detecting objects on the next animation frame
+            requestAnimationFrame(performObjectDetection);
+          }
+        );
+      }
+    };
+
+    loadModel();
+
+    return () => {
+      // Cleanup code, stop the detection when the component unmounts
+      if (objectDetectorRef.current) {
+        objectDetectorRef.current.removeListener();
+      }
+    };
+  }, []);
+
+  return (
+    <div className="bg-white w-full">
+      <div className="w-full absolute flex-col gap-4 flex items-center justify-center h-screen z-10">
+        {objects?.map?.((s, a) => {
+          return (
+            <p key={a} className=" bg-blue-500 text-white">
+              {s.label}
+            </p>
+          );
+        })}
+      </div>
+
+      <Webcam className="w-full h-screen" ref={webcamRef} muted />
+    </div>
+  );
+}
